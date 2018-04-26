ea983b839d9c758eb2c1d49fdfc3b58ce452c61d
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-04-26 20:22:17 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/codeonblue/workshopmongo/config/Instantiation.java
+++ src/main/java/com/codeonblue/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.codeonblue.workshopmongo.config;
 import com.codeonblue.workshopmongo.domain.Post;
 import com.codeonblue.workshopmongo.domain.User;
 import com.codeonblue.workshopmongo.dto.AuthorDTO;
+import com.codeonblue.workshopmongo.dto.CommentDTO;
 import com.codeonblue.workshopmongo.repository.PostRepository;
 import com.codeonblue.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().add(c3);
 
         postRepository.insert(Arrays.asList(post1, post2));
 
