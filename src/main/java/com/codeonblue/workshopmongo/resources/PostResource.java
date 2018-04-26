0b09de2a8af55e83da8cd6e4affe331444e47546
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-04-26 20:54:34 -0300

    Consulta simples com query methods

--- src/main/java/com/codeonblue/workshopmongo/resources/PostResource.java
+++ src/main/java/com/codeonblue/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.codeonblue.workshopmongo.resources;
 
 import com.codeonblue.workshopmongo.domain.Post;
+import com.codeonblue.workshopmongo.resources.util.URL;
 import com.codeonblue.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,5 +22,12 @@ public class PostResource {
         return ResponseEntity.ok().body(post);
     }
 
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 
 }
--- src/main/java/com/codeonblue/workshopmongo/services/PostService.java
+++ src/main/java/com/codeonblue/workshopmongo/services/PostService.java
@@ -27,4 +27,8 @@ public class PostService {
         return post.get();
     }
 
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
+
 }
