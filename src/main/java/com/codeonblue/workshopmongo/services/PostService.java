5760c0a37b8c464f458330a997910b723582dd17
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-04-25 23:35:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/codeonblue/workshopmongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.codeonblue.workshopmongo.services;
+
+import com.codeonblue.workshopmongo.domain.Post;
+import com.codeonblue.workshopmongo.repository.PostRepository;
+import com.codeonblue.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> findAll(){
+        return postRepository.findAll();
+    }
+
+    public Post findById(String id){
+        Optional<Post> post = postRepository.findById(id);
+        if (!post.isPresent()){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return post.get();
+    }
+
+}
