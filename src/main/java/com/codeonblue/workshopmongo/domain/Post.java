b96acff47c4dd9f4f6f867085ba0e3f00abb5af4
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-04-25 21:32:34 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/codeonblue/workshopmongo/domain/Post.java
+++ src/main/java/com/codeonblue/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.codeonblue.workshopmongo.domain;
 
+import com.codeonblue.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,17 +13,16 @@ public class Post implements Serializable {
 
     @Id
     private String id;
-
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
 
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -63,11 +63,11 @@ public class Post implements Serializable {
     }
 
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
